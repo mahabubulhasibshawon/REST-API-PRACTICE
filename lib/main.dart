3b1ade580e0fda9e64f972a110246728fcd48982
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-10-09 22:47:12 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:api_practice/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'rest api',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+

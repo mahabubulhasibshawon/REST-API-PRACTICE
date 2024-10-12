34583fcd82c2e2334ac4cc3eb0353ba40982d95a
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-10-12 23:52:00 +0600

    scrollable widget

--- lib/widgets/scrollable_widget.dart
+++ lib/widgets/scrollable_widget.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
 class ScrollableWidget extends StatelessWidget {
-  const ScrollableWidget({super.key});
+  final String headline;
+  final String ImageUrl;
+  final String ImgTxt;
+  const ScrollableWidget({super.key, required this.headline, required this.ImageUrl, required this.ImgTxt});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          Row(),
-          ListView()
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(headline, style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),),
+              Text('View all', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 12, color:  Colors.lightBlueAccent),),
+
+            ],
+          ),
+          ListView.builder(itemBuilder: (context, index) {
+            return Card(
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
+                children: [
+                  Image.network(ImageUrl, width: 150,height: 120,),
+                  Padding(padding: EdgeInsets.all(8),child: Text(ImgTxt),)
+                ],
+              ),
+            );
+          })
         ],
       ),
     );
